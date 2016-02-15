2516b8b171b3be2997ea69308cc0aeadb3814b51
Christopher Mühl <muehl@msu.biz>
2016-02-15 12:32:52 +0100

    Add FormBuilder class

---
+++ files/lib/form/FormBuilder.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace wcf\form;
+
+/**
+ * Form Builder for easier form development
+ *
+ * @author  Christopher Mühl
+ * @copyright   2016 Christopher Mühl
+ * @package     io.padarom.devtools.formbuilder
+ * @subpackage  form
+ * @category    Community Framework
+ */
+abstract class FormBuilder extends AbstractForm {
+    
+}
